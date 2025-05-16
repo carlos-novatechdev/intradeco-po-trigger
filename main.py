7f56107cf67f915f2eea1d1838b36ca773de1d10
Carlos Alberto Castillo <carlos.castillo@qualitara.com>
2025-05-16 16:29:12 -0600

    Second commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 
-def gcs_product_order():
+def gcs_product_order(event):
     # This function is a placeholder for the GCS product orderer functionality
     print("Hello, World!")
+    print(event)
